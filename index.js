4deb968862339360df62308ba2237c95b16fdd6b
eddyionescu <eddyionescu@gmail.com>
2018-03-25 18:40:33 -0400

    add skeleton for index routes

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Transit/Tech Geek!'));
 
-app.listen(1507, () => console.log('Isotransit Backend on Port 1507'))
+app.listen(1507, () => console.log('Isotransit Backend on Port 1507'));
+
+app.get('/driving', (req, res) => {
+  // TODO - make Bing Maps Isochrone API calls here
+});
+
+app.get('/transit', (req, res) => {
+  // Valhalla call here
+});
